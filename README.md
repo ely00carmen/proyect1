21fd3a75e2918ccfe7b6c8032159af1d4f45daa0
ely00carmen <eportillob@unsa.edu.pe>
2025-03-29 08:39:25 -0500

    Primer commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # proyect1
+# Proyecto de Análisis de Datos de Vehículos
+
+Este proyecto presenta una aplicación web desarrollada con **Streamlit** que permite generar un análisis exploratorio de datos de vehículos. La aplicación utiliza el conjunto de datos de anuncios de vehículos (vehicles_us.csv) y permite generar un **histograma** de la columna "odometer" y un **gráfico de dispersión** entre el odómetro y el precio del vehículo.
+
+## Funcionalidades:
+- **Histograma**: Al marcar la casilla de verificación, se construye un histograma basado en la columna "odometer".
+- **Gráfico de dispersión**: Al marcar la casilla de verificación, se construye un gráfico de dispersión que muestra la relación entre "odometer" y "price".
+
+## Requisitos:
+- **pandas**
+- **plotly**
+- **streamlit**
+
+## Cómo ejecutar:
+1. Clona el repositorio.
+2. Crea un entorno virtual y activa el entorno.
+3. Instala las dependencias: `pip install -r requirements.txt`.
+4. Ejecuta la aplicación: `streamlit run app.py`.
---
+++ app.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+import streamlit as st
+
+st.header('Gráficas')
+
+st.write('Esta aplicación aún no es funcional. En construcción.')
+
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+
+# Crear casillas de verificación para generar los gráficos
+build_histogram = st.checkbox('Construir un histograma')
+build_scatter = st.checkbox('Construir un gráfico de dispersión')
+
+hist_button = st.button('Construir histograma') # crear un botón
+if hist_button: # al hacer clic en el botón
+# escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+# Crear un botón para construir un gráfico de dispersión
+scatter_button = st.button('Construir gráfico de dispersión')
+if scatter_button:  # Cuando se hace clic en el botón para el gráfico de dispersión
+
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    
+    # Crear un gráfico de dispersión usando Plotly Express
+    fig_scatter = px.scatter(car_data, x="odometer", y="price")
+    
+    # Mostrar el gráfico de dispersión
+    st.plotly_chart(fig_scatter, use_container_width=True)
